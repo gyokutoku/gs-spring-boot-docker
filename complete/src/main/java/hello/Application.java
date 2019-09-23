cb6885cef087fbd9c161519e91e30a1721a190b9
Li, Yude <yude.li@accenture.com>
2019-09-23 11:48:23 +0900

    change repo

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
+	@RequestMapping("/")
+	public String home() {
+		logger.info("hello,word@root");
+		return "Hello Docker World";
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
